e2f685ddbb1689d81c95aea4927cec20d1b61510
Antonella Enriquez Ledesma <antonellaenriquezledesma@gmail.com>
2024-07-03 20:24:45 +0200

    add footer and navBar

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,56 @@
+
+import styled from "styled-components";
+import Facebook from "../../assets/icons/Facebook.svg";
+import Instagram from "../../assets/icons/Instagram.svg";
+
+const FooterContainer = styled.footer`
+  background-color: #ffff;
+  color: black;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  left:0px;
+  box-sizing: border-box;
+`;
+
+const FooterText = styled.p`
+  margin: 0;
+  font-size: 20px;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+
+
+  img {
+    width: 35px;
+    height: 35px;
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+`;
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterText>&copy; {new Date().getFullYear()} FreshAir. Todos los derechos reservados.</FooterText>
+      <IconContainer>
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <img src={Facebook} alt="Facebook" />
+        </a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <img src={Instagram} alt="Instagram" />
+        </a>
+      </IconContainer>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
